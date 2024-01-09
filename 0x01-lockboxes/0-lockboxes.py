110971855e317f0a025396b93ecf20df20b6f547
ELHEND <alhnd683@gmail.com>
2024-01-09 08:13:22 +0000

    zx

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,14 @@ def canUnlockAll(boxes):
     (indices) to other boxes can be unlocked given thatfirst
     box is unlocked.
     '''
-    n = len(boxes)
+    m = len(boxes)
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+        if not boxIdx or boxIdx >= m or boxIdx < 0:
             continue
         if boxIdx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    return m == len(seen_boxes)
