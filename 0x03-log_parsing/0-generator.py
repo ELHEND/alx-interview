b5567cca10f695def03999c57563da6026ce4136
ELHEND <alhnd683@gmail.com>
2024-01-23 08:41:35 -0800

    za

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-'''A script that generates random HTTP request logs.
+''' script that generates random HTTP request logs.
 '''
 import random
+
 import sys
+
 import datetime
+
 from time import sleep
 
 
