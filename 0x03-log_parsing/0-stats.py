698abbd90c1eb7b6df3084d371bd51ce6c9b38fb
ELHEND <alhnd683@gmail.com>
2024-01-23 08:39:21 -0800

    a

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''A script for parsing HTTP request logs.
 '''
+
 import re
 
 
 def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
+    '''Extracts sections of line of  HTTP request log.
     '''
     fp = (
         r'\s*(?P<ip>\S+)\s*',
@@ -29,7 +30,7 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
+    '''Prints accumulated statistics of the HTTP request log.
     '''
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
@@ -39,13 +40,13 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
+    '''Updates metrics from a given HTTP request log.
 
     Args:
-        line (str): The line of input from which to retrieve the metrics.
+        line (str):  line of input from which to retrieve the metrics.
 
     Returns:
-        int: The new total file size.
+        int: new total file size.
     '''
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
@@ -55,7 +56,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''Starts the log parser.
+    '''Starts  log parser.
     '''
     line_num = 0
     total_file_size = 0
